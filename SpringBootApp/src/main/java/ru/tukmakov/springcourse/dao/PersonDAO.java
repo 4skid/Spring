b25e52845d9774421180dac3d484af327070b02f
4skid <amgs630@gmail.com>
2024-05-14 22:27:23 +0300

    lesson 74 completed

---
+++ SpringBootApp/src/main/java/ru/tukmakov/springcourse/dao/PersonDAO.java
@@ -0,0 +1,8 @@
+package ru.tukmakov.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PersonDAO {
+
+}
---
+++ SpringBootApp/src/main/java/ru/tukmakov/springcourse/services/BookService.java
@@ -0,0 +1,91 @@
+package ru.tukmakov.springcourse.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.tukmakov.springcourse.models.Book;
+import ru.tukmakov.springcourse.models.Person;
+import ru.tukmakov.springcourse.repositories.BooksRepository;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class BookService {
+
+    private final BooksRepository booksRepository;
+
+    @Autowired
+    public BookService(BooksRepository booksRepository) {
+        this.booksRepository = booksRepository;
+    }
+
+    public List<Book> findAll(boolean sortByYear) {
+        if (sortByYear) {
+            return booksRepository.findAll(Sort.by("year"));
+        } else
+            return booksRepository.findAll();
+    }
+
+    public List<Book> findWithPagination(Integer page, Integer booksPerPage, boolean sortByYear) {
+        if (sortByYear) {
+            return booksRepository.findAll(PageRequest.of(page, booksPerPage, Sort.by("year"))).getContent();
+        } else
+            return booksRepository.findAll(PageRequest.of(page, booksPerPage)).getContent();
+    }
+
+    public Book findOne(int id) {
+        return booksRepository.findById(id).orElse(null);
+    }
+
+    public List<Book> searchByTitle(String query) {
+        return booksRepository.findByTitleStartingWith(query);
+    }
+
+    @Transactional
+    public void save(Book book) {
+        booksRepository.save(book);
+    }
+
+    @Transactional
+    public void update(int id, Book updatedBook) {
+        Book bookToBeUpdated = booksRepository.findById(id).get();
+
+        updatedBook.setId(id);
+        updatedBook.setOwner(bookToBeUpdated.getOwner());
+
+        booksRepository.save(updatedBook);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        booksRepository.deleteById(id);
+    }
+
+    public Person getBookOwner(int id) {
+        return booksRepository.findById(id).map(Book::getOwner).orElse(null);
+    }
+
+    @Transactional
+    public void release(int id) {
+        booksRepository.findById(id).ifPresent(
+                book -> {
+                    book.setOwner(null);
+                    book.setTakenAt(null);
+                }
+        );
+    }
+
+    @Transactional
+    public void assign(int id, Person selectedPerson) {
+        booksRepository.findById(id).ifPresent(
+                book -> {
+                    book.setOwner(selectedPerson);
+                    book.setTakenAt(new Date());
+                }
+        );
+    }
+}
